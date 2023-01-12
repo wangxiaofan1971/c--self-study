11b94e015c42dca53b47ee85b5c191c8caedf8ee
laozei <83691927+wangxiaofan1971@users.noreply.github.com>
2023-01-12 10:52:05 +0800

    Rename 47 选择结构-switch语句.cpp to 407 选择结构-switch语句.cpp

---
+++ 407 选择结构-switch语句.cpp
@@ -0,0 +1,29 @@
+#include<iostream>
+using namespace std;
+int main7()
+{
+	int score = 0;
+	cout << "请输入电影评分:" << endl;
+	cin >> score;
+	cout << "您打的分数为：" << score << endl;
+	switch (score)
+	{
+	case 10:
+		cout << "您认为是经典电影" << endl; 
+		break;
+	case 9:
+		cout << "您认为是经典电影" << endl; 
+		break;
+	case 8:
+		cout << "您认为是好看电影" << endl; 
+		break;
+	case 7:
+		cout << "您认为是一般电影" << endl; 
+		break;
+	default:
+		cout << "您认为是烂片" << endl;
+		break;
+	}
+	system("pause");
+	return 0;
+}
